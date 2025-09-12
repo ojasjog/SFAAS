0664da3f6b0191edcee117d3df7e03abbd793f96
Ojas <ojasmjog@gmail.com>
2025-09-12 23:23:28 +0530

    Created forecasts.json for the admin menu option 1 (Adding Seasonal Forecasts). Terminal output needs formatting

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import time
 from rich.console import Console
 console = Console()
+import json
+import os
+from datetime import datetime   
 
 def admin_login():
-    print("1. Hello")
+    print("1. Add Seasonal Forecast")
     print("2. Bye")
 
 def farmer_login():
@@ -24,9 +27,67 @@ if choice == "1":
         console.log(f"Doing important stuff...{i}")
         time.sleep(0.25)
     admin_login()
+
+    #Admin menu begins here
     choice = input("Enter choice: ")
     if choice=="1":
-        print("ok")
+        import json
+
+
+        DATA_FILE = "forecasts.json"
+
+        # Read file into list
+        def read_data():
+            if os.path.exists(DATA_FILE):
+                with open(DATA_FILE, "r") as f:
+                    try:
+                        return json.load(f)
+                    except json.JSONDecodeError:
+                        return []
+            return []
+
+        # Save list back to file
+        def write_data(data):
+            with open(DATA_FILE, "w") as f:
+                json.dump(data, f, indent=2)
+
+        def save_entry(entry):
+            data = read_data()
+            data.append(entry)
+            write_data(data)
+            print("✅ Saved:", entry)
+
+        if __name__ == "__main__":
+            # Take inputs from user
+            season = input("Enter season: ")
+            region = input("Enter region: ")
+            start_date = input("Enter start date: ")
+            end_date=input("Enter end date: ")
+            rainfall=input("Enter rainfall(in mm): ")
+            temperature=input("Enter temperature(in °C): ")
+            humidity=input("Enter humidity(in %): ")
+            crop_suggestions=input("Enter crop suggestions: ")
+            pest_alert=input("Enter pest alert: ")
+            advisory_notes=input("Enter advisory notes: ")
+
+
+            # Create an entry dictionary
+            entry = {
+                "season": season,
+                "region": region,
+                "dates": {"start": start_date, 
+                          "end": end_date},
+                "weather_forecast": {
+                    "rainfall": rainfall, "temperature": temperature, "humidity": humidity
+                },
+                "crop_suggestions": list(crop_suggestions.split(",")),
+                "pest_alert": list(pest_alert.split(",")),
+                "advisory_notes": advisory_notes,
+                "timestamp": datetime.now().isoformat()
+            }
+
+            # Save entry dictionary to file
+            save_entry(entry)     
 
 elif choice == "2":
     for i in range(1,4):
