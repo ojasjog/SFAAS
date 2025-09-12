f3ad53ca7220ef58aae6ca9072d1898acf8c4d52
APTXN47 <ashmitpandey2k@gmail.com>
2025-09-13 00:36:07 +0530

    Main Menu Updated

--- main.py
+++ main.py
@@ -3,13 +3,60 @@ from rich.console import Console
 console = Console()
 
 def admin_login():
-    print("1. Hello")
-    print("2. Bye")
+    print("\n--- Admin Menu ---")
+    print("1. Add Seasonal Forecast")
+    print("2. Update/Delete Forecast")
+    print("3. Manage Forecast")
+    print("4. Upload Bulk Forecast Data")
+    print("5. Generate Reports")
+    print("6. Manage Farmer Queries")
+    print("7. Logout")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        print("Loading 1")
+    elif choice == "2":
+        print("Loading 2")
+    elif choice == "3":
+        print("Loading 3")
+    elif choice == "4":
+        print("Loading 4")
+    elif choice == "5":
+        print("Loading 5")
+    elif choice == "6":
+        print("Loading 6")
+    elif choice == "7":
+        print("Logging out...")
+    else:
+        print("Invalid choice.")
+
 
 def farmer_login():
-    print("1. Siu")
-    print("2. oo")
+    print("\n--- Farmer Menu ---")
+    print("1. View Seasonal Forecast")
+    print("2. Access Crop Advisories")
+    print("3. Search Historical Data")
+    print("4. Submit Query/Request Advisory")
+    print("5. Manage Profile")
+    print("6. Logout")
 
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        print("Loading 1")
+    elif choice == "2":
+        print("Loading 2")
+    elif choice == "3":
+        print("Loading 3")
+    elif choice == "4":
+        print("Loading 4")
+    elif choice == "5":
+        print("Loading 5")
+    elif choice == "6":
+        print("Logging out...")
+    else:
+        print("Nothing updated")
 
 print("\n== Seasonal Forecast & Agriculture Advisory System ===")
 print("1. Admin Login")
@@ -20,30 +67,27 @@ print("4. Exit")
 choice = input("Enter choice: ")
 
 if choice == "1":
-    for i in range(1,4):
-        console.log(f"Doing important stuff...{i}")
+    for i in range(1, 4):
+        console.log(f"Loading data...{i}")
         time.sleep(0.25)
     admin_login()
-    choice = input("Enter choice: ")
-    if choice=="1":
-        print("ok")
+
 
 elif choice == "2":
-    for i in range(1,4):
-        console.log(f"Doing important stuff...{i}")
+    for i in range(1, 4):
+        console.log(f"Loading data...{i}")
         time.sleep(0.25)
     farmer_login()
-    choice = input("Enter choice: ")
-    if choice=="1":
-        print("okk")
+
 elif choice == "3":
-    for i in range(1,4):
-        console.log(f"Doing important stuff...{i}")
+    for i in range(1, 4):
+        console.log(f"Loading data...{i}")
         time.sleep(0.25)
     print("Registration")
+
 elif choice == "4":
-    print("Loaded. Goodbye!")
+    print("Closed succesfully.")
 
 else:
     print("Invalid choice. Try again.")
-    
+
